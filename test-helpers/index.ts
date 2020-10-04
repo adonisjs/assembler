88ca66ffb6e2ee609214f4f17df6600f14b58483
Harminder Virk <virk.officials@gmail.com>
2020-10-04 16:02:09 +0530

    test: do not add colors functions in CI output

--- test-helpers/index.ts
+++ test-helpers/index.ts
@@ -15,4 +15,4 @@ export const info = process.env.CI ? '[ info ]' : '[ blue(info) ]'
 export const success = process.env.CI ? '[ success ]' : '[ green(success) ]'
 export const error = process.env.CI ? '[ error ]' : '[ red(error) ]'
 export const warning = process.env.CI ? '[ warn ]' : '[ yellow(warn) ]'
-export const dimYellow = (value: string) => `dim(yellow(${value}))`
+export const dimYellow = (value: string) => (process.env.CI ? value : `dim(yellow(${value}))`)
